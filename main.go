e8dc36f98f16dbb3a8941620f67d24f887920f5a
João Pedro <jpedro1580@gmail.com>
2024-09-16 18:40:15 -0300

    refactor(repl): refactor main loop to its own package

--- main.go
+++ main.go
@@ -1,31 +1,9 @@
 package main
 
 import (
-	"bufio"
-	"fmt"
-	"os"
-	"strings"
-
-	"github.com/JP-Go/pokedex-go/internal"
-	"github.com/JP-Go/pokedex-go/internal/commands"
+	"github.com/JP-Go/pokedex-go/internal/repl"
 )
 
 func main() {
-
-	scanner := bufio.NewScanner(os.Stdin)
-	commandHandler := commands.NewCommandHandler()
-	for {
-		internal.PrintPrompt()
-		if scanner.Scan() {
-			input := strings.TrimSpace(scanner.Text())
-			handler, err := commandHandler.GetCommand(input)
-			if err != nil {
-				fmt.Println("Invalid command " + input)
-			}
-			err = handler.Callback()
-			if err != nil {
-				panic(fmt.Sprintf("Error with command %s: %v", input, err))
-			}
-		}
-	}
+	repl.StartRepl()
 }
